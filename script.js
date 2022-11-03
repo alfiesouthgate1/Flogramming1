64fb93178345cb41569a6b31f13d5694c9d032d4
alfiesouthgate1 <alfiesouthgate@outlook.com>
2022-11-03 20:56:55 +0000

    Completed the Contact form

--- script.js
+++ script.js
@@ -8,30 +8,44 @@ const errorNodes = document.querySelectorAll(".error");
 
 //validate data
 function validateForm(){
-    console.log("Hello World!!!")
     clearMessages();
+    let errorFlag = false;
 
-
-    if(nameInput.value.length < 1){
+    if (nameInput.value.length < 1) {
         errorNodes[0].innerText = "Name cannot be blank";
         nameInput.classList.add("error-border");
+        errorFlag = true;
     }
-    if(!emailIsValid(email.value)){
+    if(!emailIsValid(email.value)) {
         errorNodes[1].innerText = "Invalid email address";
         email.classList.add("error-border");
+        errorFlag = true;
     }
-}
-//Clear error or success messages
+    if(message.value.length <1) {
+        errorNodes[2].innerText = "Please enter a message";
+        email.classList.add("error-border");
+        errorFlag = true;
+    }
+    if (!errorFlag) {
+        success.innerText = "Success!";
+        console.log(message.value);
+    }
+
+
+// Clear error or success messages
 function clearMessages(){
-    for(let i = 0; i < errorNodes.length; i=i++){
+    for(let i=0; i < errorNodes.length; i++){
         errorNodes[i].innerText = "";
     }
+    success.innerText = "";
     nameInput.classList.remove("error-border");
     email.classList.remove("error-border");
+    message.classList.remove("error-border");
 }
-
 //Check if email is valid
 function emailIsValid(email){
-    let pattern = /\S+@\.\S+/;
-    return pattern.test(email);
+    let pattern = /\S+@\S+\.\S+/;
+    return pattern.test(email)}
 }
+
+
